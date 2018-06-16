56b2e698ab2cd9fd36c51f8fcc2d773d7d477d21
Raphael Hippe <info@raphaelhippe.de>
2018-06-16 15:27:28 +0200

    works as standalone

---
+++ src/main/java/de/hska/iwi/vslab/authorizationserver/AuthorizationServerController.java
@@ -0,0 +1,26 @@
+package de.hska.iwi.vslab.authorizationserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.Principal;
+
+@RestController
+public class AuthorizationServerController {
+  // @RequestMapping( value = "/login")
+  // public Principal userInfo(@AuthenticationPrincipal Principal user){
+  //   return user;
+  // }
+
+
+  @GetMapping( value = "index")
+  public String index(){
+    return "this is open";
+  }
+
+  @GetMapping( value = "secureAPI")
+  public String secureAPI(){
+    return "this is secure";
+  }
+}
---
+++ src/main/java/de/hska/iwi/vslab/authorizationserver/SecurityConfiguration.java
@@ -0,0 +1,60 @@
+package de.hska.iwi.vslab.authorizationserver;
+
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+      return super.userDetailsServiceBean();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth.inMemoryAuthentication()
+        .withUser("barry").password("{noop}password").roles("USER")
+        .and()
+        .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http.httpBasic()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/index").permitAll()
+        .anyRequest().authenticated();
+      http.csrf().disable();
+
+        // http.requestMatchers()
+        //   .antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
+        //   .and()
+        //   .formLogin().loginPage("/login").permitAll().failureUrl("/login?error")
+        //   .and()
+        //   .authorizeRequests().anyRequest().authenticated();
+    }
+
+// barry:password
+// Basic YmFycnk6cGFzc3dvcmQ=
+
+
+}
